ed27cfdad776730aa74bf84fcf6742534a37f825
Sarah Abumandil <114596306+Sarah111-AHM@users.noreply.github.com>
2026-01-11 08:15:30 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,6 @@
 """
-media advisor expert system
-Version 1.0.0
-this project implements a rule-based expert system that recommends
-appropriate educational media based on user needs and constraints, includes a clear conflict resolution strategy to handle
-overlapping or competing rules
+media advisor expert system | Version 1.0.0
 """
-
 __version__ = "1.0.0"
 __author__ = "DSAI 3302 Student"
 __description__ = "rule based expert system for educational media recommendation with conflict resolution support"
